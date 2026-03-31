4a5248aaa2cf5f875136f50b385794505c72fa81
minhuyi <minhhuy5929@gmail.com>
2026-03-31 21:08:19 +0700

    add backend Spring Boot

---
+++ backend/src/main/java/com/blogforum/BlogforumApplication.java
@@ -0,0 +1,13 @@
+package com.blogforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogforumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blogforum/BlogforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
